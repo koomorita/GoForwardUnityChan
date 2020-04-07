96e338f33e16d3a0619fcf4c371c821ddc49c6e7
koomorita <doraemon-.-love@ezweb.ne.jp>
2020-04-07 22:18:10 +0900

    Lesson7　課題提出　GoForwardUnityChan
    ブロック同士が当たった時、ブロックが地面に当たった時に効果音がなるようにしました。

---
+++ Assets/CubeController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class CubeController : MonoBehaviour
+{
+    public AudioClip sound1;
+    AudioSource audioSource;
+    
+
+    // キューブの移動速度
+    private float speed = -12;
+
+    // 消滅位置
+    private float deadLine = -10;
+
+    // Use this for initialization
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        GameObject ground = GameObject.Find("BG");
+        GameObject CubePrefab = GameObject.Find("cube");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+
+
+        // キューブを移動させる
+        transform.Translate(this.speed * Time.deltaTime, 0, 0);
+
+        // 画面外に出たら破棄する
+        if (transform.position.x < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        //地面に衝突したか、ブロック同士で衝突したか。
+
+        if (collision.gameObject.tag == "BG")
+        {
+            audioSource.PlayOneShot(sound1);
+
+        }
+
+        if (collision.gameObject.tag == "cube")
+        {
+            audioSource.PlayOneShot(sound1);
+
+        }
+
+    }
+
+}
